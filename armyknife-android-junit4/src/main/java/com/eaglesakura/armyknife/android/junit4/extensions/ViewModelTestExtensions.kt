7d6b941cb733d1650493bb3e18040a00c023384f
eaglesakura <eagle.sakura@gmail.com>
2019-07-05 18:33:39 +0900

    add: ViewModel.activeAllLiveDataForTest added

--- armyknife-android-junit4/src/main/java/com/eaglesakura/armyknife/android/junit4/extensions/ViewModelTestExtensions.kt
+++ armyknife-android-junit4/src/main/java/com/eaglesakura/armyknife/android/junit4/extensions/ViewModelTestExtensions.kt
@@ -1,15 +1,22 @@
 package com.eaglesakura.armyknife.android.junit4.extensions
 
+import android.util.Log
 import androidx.annotation.IdRes
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.jvm.isAccessible
 
 /**
  * Make testing ViewModel without View.
@@ -114,5 +121,24 @@ suspend fun <VM : ViewModel> makeFragmentViewModel(clazz: KClass<VM>): VM {
 suspend fun <VM : ViewModel> makeFragmentSavedStateViewModel(clazz: KClass<VM>): VM {
     return makeFragmentViewModel { fragment ->
         ViewModelProviders.of(fragment, SavedStateViewModelFactory(fragment)).get(clazz.java)
+    }
+}
+
+/**
+ * ViewModel.LiveData<*> field are active.
+ */
+@UiThread
+fun ViewModel.activeAllLiveDataForTest(lifecycleOwner: LifecycleOwner? = null) {
+    val self = this
+    self.javaClass.kotlin.declaredMemberProperties.forEach { prop ->
+        prop.isAccessible = true
+        (prop.getter.call(self) as? LiveData<*>)?.also { liveData ->
+            Log.d("ViewModelTest", "${self.javaClass.simpleName}.${prop.name} is active")
+            if (lifecycleOwner != null) {
+                liveData.observe(lifecycleOwner, Observer { })
+            } else {
+                liveData.observeForever { }
+            }
+        }
     }
 }
